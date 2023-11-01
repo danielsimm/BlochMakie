bd3a57a8fa37336c8ed313dd2ad13eaaf826e638
danielsimm <daniel.simm@outlook.com>
2023-11-01 22:44:30 +0100

    docs and cleanup

--- BlochMakie.jl
+++ BlochMakie.jl
@@ -2,9 +2,14 @@ using GLMakie
 using LinearAlgebra
 using ColorSchemes
 
-include("utils/bloch_utils.jl")
+include("utils/bloch_utils.jl") # TODO: fix phi projection
 include("utils/vis_utils.jl")
 
+"""
+    bloch_preview(ϕ, θ, size=1000; cs=default_colorscheme, vec_color=default_vectorcolor)
+
+    Creates a Makie Figure of the Bloch sphere with the vector at the angle defined by `ϕ` and `θ`. Optional `size` for the resolution of the image. Optional `cs` for the color scheme of the sphere. Optional `vec_color` for the color of the vector
+"""
 function bloch_preview(ϕ, θ, size=1000; cs=default_colorscheme, vec_color=default_vectorcolor)
     fig = Figure(resolution = (size, size))
     ax = Axis3(fig[1, 1], aspect=(1,1,1), azimuth=0.2, elevation=pi/8, perspectiveness=0.0)
@@ -45,7 +50,12 @@ function bloch_preview(ϕ, θ, size=1000; cs=default_colorscheme, vec_color=defa
         
     display(fig)
 end
-    
+
+"""
+    bloch_animation_png(ϕs, θs, name, size=1000; cs=default_colorscheme, vec_color=default_vectorcolor)
+
+    Saves a series of png images with alpha channel of the Bloch sphere with the vector moving along the path defined by `ϕs` and `θs`. Required `name` for the folder to save the images in. Optional `size` for the resolution of the images. Optional `cs` for the color scheme of the sphere. Optional `vec_color` for the color of the vector
+"""
 function bloch_animation_png(ϕs, θs, name, size=1000; cs=default_colorscheme, vec_color=default_vectorcolor)
     if length(ϕs) != length(θs)
         error("ϕs and θs must have the same length")
@@ -112,7 +122,6 @@ function bloch_animation_png(ϕs, θs, name, size=1000; cs=default_colorscheme, 
         theta_text_obs[] = Point3f(angle_to_cartesian(ϕ, θ/2))
         save("$(name)/frame_$(t).png", alpha_colorbuffer(fig))
     end
-
 end
 
 function bloch_animation_preview(ϕs, θs, size=1000; cs=default_colorscheme, vec_color=default_vectorcolor)
@@ -123,7 +132,7 @@ function bloch_animation_preview(ϕs, θs, size=1000; cs=default_colorscheme, ve
     θ = θs[1]
 
     fig = Figure(resolution = (size, size))
-    ax = Axis3(fig[1, 1], aspect=(1,1,1), azimuth=0.2, elevation=pi/8, perspectiveness=0.0)
+    ax = Axis3(fig[1, 1], aspect=(1,1,1), azimuth=(pi/2 - 0.1), elevation=pi/8, perspectiveness=0.0)
     hidedecorations!(ax)
     hidespines!(ax)
 
@@ -159,17 +168,8 @@ function bloch_animation_preview(ϕs, θs, size=1000; cs=default_colorscheme, ve
     text!(ax, Point3f(1.35, 0, 0); text=L"X", color=:black, fontsize=fs2)
     text!(ax, Point3f(0, 1.35, 0); text=L"Y", color=:black, fontsize=fs2)
 
-    
-    # define observables
-    phi_proj_obs = Observable(Point3f.(phi_projection(ϕ, θ)...))
-    theta_proj_obs = Observable(Point3f.(theta_projection(ϕ, θ)...))
-    pos_obs = Observable([Point3f(angle_to_cartesian(ϕ, θ)...)])
-    z_proj = Observable([Point3f.(angle_to_cartesian(ϕ, θ)...), Point3f.(angle_to_cartesian(ϕ, θ)[1], angle_to_cartesian(ϕ, θ)[2], 0.0)])
-    phi_text_obs = Observable(Point3f(phi_text_pos(ϕ, θ)...))
-    theta_text_obs = Observable(Point3f(angle_to_cartesian(ϕ, θ/2)))
-
     display(fig)
-    sleep(1)
+    sleep(2)
 
     for t in eachindex(ϕs)
         ϕ = ϕs[t]
@@ -182,7 +182,4 @@ function bloch_animation_preview(ϕs, θs, size=1000; cs=default_colorscheme, ve
         theta_text_obs[] = Point3f(angle_to_cartesian(ϕ, θ/2))
         sleep(1/60)
     end
-
 end
-
-bloch_animation_preview(collect(0:0.1:2pi), collect(0:0.1:2pi), 1000)
